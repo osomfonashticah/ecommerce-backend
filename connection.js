dc16210b60c9e6f1295617c5a984e9e9d9e943da
osomfonashticah <osomfonashticah@gmail.com>
2022-11-22 23:17:49 +0000

    initial commit

---
+++ connection.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.DB_URL, { useNewUrlparser: true })
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.log(err));
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
