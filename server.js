48e4b74f243494f68fb1be895837c4d06c5a5a93
osomfonashticah <osomfonashticah@gmail.com>
2022-11-23 12:54:11 +0000

    changes to server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require("./connection");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
-  cors: "http://localhost:3000",
+  cors: "https://ecommerce-7ffe7.web.app/",
   methods: ["GET", "POST", "PATCH", "DELETE"],
 });
 
